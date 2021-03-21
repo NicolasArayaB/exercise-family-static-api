707d29b8efd5744f3df7e5039ae955588ff79585
NicolasArayaB <nicolasaraya@gmail.com>
2021-03-21 14:56:55 +0000

    test approved

--- src/datastructures.py
+++ src/datastructures.py
@@ -13,23 +13,40 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [{"first_name": "John", "id": self._generateId(), "age": 33, "lucky_numbers": [7, 13, 22]},
+        {"first_name": "Jane", "id": self._generateId(), "age": 35, "lucky_numbers": [10, 14, 3]},
+        {"first_name": "Jimmy", "id": self._generateId(), "age": 5, "lucky_numbers": [1]}]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
+        # Tommy will have a static id
+        if member["first_name"] == "Tommy":
+            tommy = {"first_name": str(member["first_name"]), "id": member["id"], "age": int(member["age"]), "lucky_numbers": member["lucky_numbers"]}
+            self._members.append(tommy)
+            return
+        else:
         # fill this method and update the return
-        pass
+            new_member = {"first_name": str(member["first_name"]),"id": self._generateId(), "age": int(member["age"]), "lucky_numbers": member["lucky_numbers"]}
+            self._members.append(new_member)
+            return
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+        for member in self._members:
+            print(member["id"])
+            if member["id"] == id:
+                print("YEY")
+                self._members.remove(member)
+        return
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        for member in self._members:
+            if member["id"] == id:
+                return member
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
